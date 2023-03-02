0841e812732117dfde47825413e1a92db2a1cfda
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-03-02 18:37:17 -0300

    Subindo o projeto para exercício do modulo 17

---
+++ UpdateTracker/UpdateClock.cs
@@ -0,0 +1,16 @@
+﻿using Cinemachine;
+using System;
+
+namespace UpdateTracker
+{
+    internal class UpdateClock
+    {
+        public static UpdateClock Late { get; internal set; }
+        public static object Fixed { get; internal set; }
+
+        public static explicit operator UpdateClock(CinemachineCore.UpdateFilter v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
