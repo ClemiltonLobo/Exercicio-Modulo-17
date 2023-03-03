5c1a6945d26c4dded7658ad57d2ca58d8d92948c
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-03-02 23:40:34 -0300

    Continuando a escrever o script de vida e dano

--- Assets/Scripts/Health/LifeBase.cs
+++ Assets/Scripts/Health/LifeBase.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeBase : MonoBehaviour
 {
+    public Image filledLife;
+    public int currentLife;
+    public int fullLife = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentLife = fullLife;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+               
+    }
+    public void TakesDamage()
+    {
+        currentLife -= 10;
+
+        if (currentLife <= 0)
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
