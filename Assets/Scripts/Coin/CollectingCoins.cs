2fab420a04957d920094c405e641694ddc432bf9
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-03-03 23:31:53 -0300

    alterando e adicionando novas funcoes no script coins, e criando uma nova tag "Vida"

--- Assets/Scripts/Coin/CollectingCoins.cs
+++ Assets/Scripts/Coin/CollectingCoins.cs
@@ -8,28 +8,45 @@ public class CollectingCoins : MonoBehaviour
 {
 
     public TMP_Text scoreTxt;
+    public TMP_Text lifePlayer;
     private int score;
+    private int life;
+
+    public HealthBase healthBase;
 
 
     // Start is called before the first frame update
     private void Start()
     {
         score = 0;
+        life = healthBase.startLife;
+        UpdateScoreAndLifeTexts();
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        scoreTxt.text = score.ToString();
+        UpdateScoreAndLifeTexts();
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.CompareTag("Moeda")==true)
+        if (col.CompareTag("Moeda"))
         {
-            score = score +1;
+            score++;
             Destroy(col.gameObject);
         }
+        else if (col.CompareTag("Vida"))
+        {
+            life++;
+            Destroy(col.gameObject);
+        }
+    }
+
+    private void UpdateScoreAndLifeTexts()
+    {
+        scoreTxt.text = "Score: " + score.ToString();
+        lifePlayer.text = "Life: " + life.ToString();
     }
 }
